85294820eefaaac4e046a14c90fcadc952e57e9e
Mallory Feuer <mallory.feuer@gmail.com>
2018-01-30 16:26:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
-  if (int % 3) == 0 && (int % 5) > 0
+  if (int % 3 == 0) && (int % 5 > 0)
     puts "Fizz"
-  end
-  elsif (int % 3) > 0 && (int % 5) == 0
+  
+  elsif (int % 3 > 0) && (int % 5 == 0)
     puts "Buzz"
-  end
+  
   elsif int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
-  end
+  
   else
     return nil
-  end
+  
 end
